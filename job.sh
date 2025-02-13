346cf2ca30c82bc04769a72b07e4c6b28aed7781
gtseligas <tseligasgio@gmail.com>
2025-02-13 22:49:48 +0100

    try different job/train configs

--- job.sh
+++ job.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
  
 #SBATCH --nodes=2                    # 1 node
-#SBATCH --ntasks-per-node=1         # 32 tasks per node
+#SBATCH --ntasks-per-node=1      
+#SBATCH --cpus-per-task=32           # 32 threads per node
 #SBATCH --time=0:30:00               # time limit: 1 hour
 #SBATCH --error=train.err            # standard error file
 #SBATCH --output=train.out           # standard output file
@@ -14,4 +15,10 @@ source $WORK/oumi-dev-env/bin/activate
 
 cd $HOME/llama-train
 
-oumi train -c train.yaml
+if [ x$SLURM_CPUS_PER_TASK == x ]; then
+  export OMP_NUM_THREADS=1
+else
+  export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+fi
+
+oumi distributed torchrun -m oumi train -c train.yaml
